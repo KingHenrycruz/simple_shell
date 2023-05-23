0f4a984888a427c41d016028b18f41d04f54b1a8
KingHenrycruz <KingHenrycruz@gmail.com>
2023-05-23 11:20:47 -0700

    from KingHenry

---
+++ builtins.c
@@ -0,0 +1,39 @@
+#include "builtins.h"
+
+/**
+ * get_builtins - to retrieve the bulitins
+ * Return: points to a terminated NULL of builtins
+ */
+const builtin_t *get_builtins(void)
+{
+	static builtin_t builtins[] = {
+		{"alias", __alias, ALIAS_HELP, ALIAS_DESC},
+		{"cd", __cd, CD_HELP, CD_DESC},
+		{"env", __env, ENV_HELP, ENV_DESC},
+		{"exec", __exec, EXEC_HELP, EXEC_DESC},
+		{"exit", __exit, EXIT_HELP, EXIT_DESC},
+		{"help", __help, HELP_HELP, HELP_DESC},
+		{"setenv", __setenv, SETENV_HELP, SETENV_DESC},
+		{"unsetenv", __unsetenv, UNSETENV_HELP, UNSETENV_DESC},
+		{0}
+	};
+
+	return (builtins);
+}
+
+/**
+ * get_builtin - retrieve a builtin through it's name
+ * @name: builtin title/name to retrieve
+ * Return: pointer to builtin. but if no match is found, then NULL
+ */
+const builtin_t *get_builtin(const char *name)
+{
+	const builtin_t *builtin = NULL;
+
+	for (builtin = get_builtins(); builtin->name; builtin += 1)
+	{
+		if (_strcmp(name, builtin->name) == 0)
+			return (builtin);
+	}
+	return (NULL);
+}
