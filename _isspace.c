7b56ab78e28fffa34f8d2912f1b0a401721a7558
Tivah <tivakudzezishiri@gmail.com>
2023-05-26 06:46:10 -0700

    _isspace.c

---
+++ _isspace.c
@@ -0,0 +1,11 @@
+#include "ctype.h"
+
+/**
+ * _isspace - identify whitespaced charactes.
+ * @c identified charater
+ * Return: Could c be whitespaced, return 1. Otherwise, return 0.
+ */
+bool _isspace(int c);
+{
+return (c == ' ' || (c >= 0x09 && c <= 0x0d));
+}
