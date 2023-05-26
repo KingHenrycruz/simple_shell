8f0c0aa74caf8fe023c67a0cdfd961ef8d4f6d02
KingHenrycruz <KingHenrycruz@gmail.com>
2023-05-26 04:11:00 -0700

    from KingHenry

---
+++ _isalnum.c
@@ -0,0 +1,12 @@
+#include "ctype.h"
+
+/**
+ * _isalnum: alphanumeric char checker
+ * @c: The character to be checked.
+ * Return: 1 if the character is alphanumeric, 0 if otherwise.
+ */
+
+bool _isalnum(int c)
+{
+	return (_isdigit(c) || _isalpha(c));
+}
---
+++ _isalpha.c
@@ -0,0 +1,12 @@
+#include "ctype.h"
+
+/**
+ * _isalpha - Determines if the character is an alphabetic character.
+ * @c: The character to be checked.
+ * Returns: 1 if the character is alphabetic, 0 otherwise.
+ */
+
+bool _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
