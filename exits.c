1d7d58d25356f54a88909ba0f58ccdc438a1d000
KingHenrycruz <KingHenrycruz@gmail.com>
2023-05-26 16:23:45 -0700

    from KingHenry

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - To copy a str
+ *@dest: Dest of str
+ *@src: Where str copied from
+ *@n: char qty to be copied
+ *Return: Str after concatenation
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - Joins strings (2)
+ *@dest: Joined to
+ *@src: Joined from
+ *@n: memory used
+ *Return: string concaced
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - Locate str char
+ *@s: String inputted
+ *@c: Char to be found
+ *Return: Memory space
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
